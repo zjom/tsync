8ebf854db804aac9cad1daff1a27b3c8f4ddd7d1
Zihan Jin <admin@zihanjin.com>
2026-02-06 12:35:26 +1100

    chore: rename core/task to core/tasks
    a

--- pkg/core/tasks/taskService.go
+++ pkg/core/tasks/taskService.go
@@ -1,14 +1,14 @@
-package task
+package tasks
 
-type TaskListService interface {
+type Service interface {
+	// task lists
 	CreateTaskList(*TaskList) (*TaskList, error)
 	ListTaskLists() ([]*TaskList, error)
 	GetTaskList(listId string) (*TaskList, error)
 	UpdateTaskList(*TaskList) (*TaskList, error)
 	DeleteTaskList(listId string) error
-}
 
-type TaskService interface {
+	// task items
 	CreateTask(listId string, t *Task) (*Task, error)
 	GetTask(taskId string) (*Task, error)
 	UpdateTask(t *Task) (*Task, error)
--- pkg/core/tasks/tasklist.go
+++ pkg/core/tasks/tasklist.go
@@ -1,4 +1,4 @@
-package core
+package tasks
 
 type TaskList struct {
 	Id    string
